70d41589c800cc50deb5bc653bebd3e995e48aed
Diego Lopez Grisanti <diegolopezgrisanti@gmail.com>
2021-04-30 19:32:00 -0300

    [ADD] Nitrogen base of valid DNA, matrix NxN

--- src/controllers/mutation.controller.js
+++ src/controllers/mutation.controller.js
@@ -17,6 +17,23 @@ class MutationController {
                 error.status = 400;
                 throw error;
             }
+            for(let i = 0; i < (req.body.dna).length; i++) {
+                if((req.body.dna).length != (req.body.dna)[i].length) {
+                    let error = new Error("Wrong array dna (NxN)");
+                    error.status = 400;
+                    throw error;
+                }
+            }
+            for(let i = 0; i < (req.body.dna).length; i++) {
+                for(let j = 0; j < (req.body.dna)[i].length ; j++) {
+                    if((req.body.dna)[i][j] != "A" && (req.body.dna)[i][j] != "C" 
+                    && (req.body.dna)[i][j] != "G" && (req.body.dna)[i][j] != "T") {
+                        let error = new Error("The nitrogenous base of DNA is invalid");
+                        error.status = 400;
+                        throw error;
+                    }    
+                }
+            }
 
             const hasMutation = await this._mutationService.processMutation(req.body.dna);
 
